9f267d51a0571398445a310228c54989c6f57b18
Bashiru Saliyatu <66222696+femmeverteuse@users.noreply.github.com>
2020-06-04 08:39:46 -0700

    ochanya-HNG-00676 script

---
+++ scripts/ochanya-HNG-00676.js
@@ -0,0 +1,7 @@
+const text = "Hello World," 
+const fullname = "Bashiru Saliyatu" 
+const ID ="HNG-00676" 
+const email = "ochanyaanthony@gmail.com" 
+const language = "JavaScript" 
+const result = (text + " " + "this is" + " " + fullname + " " + "with HNGi7 ID" + " " + ID + " " + "and email" + " "+ email + " " + "using" + " " + language + " " + "for stage 2 task")  
+console.log(result)
